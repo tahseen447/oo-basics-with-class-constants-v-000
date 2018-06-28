94e87650363a1fddf0cdf024d9b60ea90d585187
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-28 21:09:03 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
 
   GENRES = []
@@ -10,6 +10,9 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=
   end
 
 end
